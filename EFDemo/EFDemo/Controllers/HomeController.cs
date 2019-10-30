b116b7f0ce229eb64c41bb8505479770b826b6ec
Benjamin Yu <byu@bcit.ca>
2019-10-30 08:40:11 -0700

    Add files via upload

---
+++ EFDemo/EFDemo/Controllers/HomeController.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using EFDemo.Models;
+
+namespace EFDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly pubsContext _context;
+
+        public HomeController(pubsContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Home
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Authors.ToListAsync());
+        }
+
+        // GET: Home/Details/5
+        public async Task<IActionResult> Details(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var authors = await _context.Authors
+                .FirstOrDefaultAsync(m => m.AuId == id);
+            if (authors == null)
+            {
+                return NotFound();
+            }
+
+            return View(authors);
+        }
+
+        // GET: Home/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Home/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("AuId,AuLname,AuFname,Phone,Address,City,State,Zip,Contract")] Authors authors)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(authors);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(authors);
+        }
+
+        // GET: Home/Edit/5
+        public async Task<IActionResult> Edit(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var authors = await _context.Authors.FindAsync(id);
+            if (authors == null)
+            {
+                return NotFound();
+            }
+            return View(authors);
+        }
+
+        // POST: Home/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(string id, [Bind("AuId,AuLname,AuFname,Phone,Address,City,State,Zip,Contract")] Authors authors)
+        {
+            if (id != authors.AuId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(authors);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!AuthorsExists(authors.AuId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(authors);
+        }
+
+        // GET: Home/Delete/5
+        public async Task<IActionResult> Delete(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var authors = await _context.Authors
+                .FirstOrDefaultAsync(m => m.AuId == id);
+            if (authors == null)
+            {
+                return NotFound();
+            }
+
+            return View(authors);
+        }
+
+        // POST: Home/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(string id)
+        {
+            var authors = await _context.Authors.FindAsync(id);
+            _context.Authors.Remove(authors);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool AuthorsExists(string id)
+        {
+            return _context.Authors.Any(e => e.AuId == id);
+        }
+    }
+}
---
+++ EFDemo/EFDemo/Models/PubInfo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EFDemo.Models
+{
+    public partial class PubInfo
+    {
+        public string PubId { get; set; }
+        public byte[] Logo { get; set; }
+        public string PrInfo { get; set; }
+
+        public Publishers Pub { get; set; }
+    }
+}
